760225afe6a16ef0a617c32dc3ad511a0be761d0
Totoro700 <68616592+Totoro700@users.noreply.github.com>
2020-11-16 14:42:51 -0600

    Updated the spam section

--- BotFlip.cpp
+++ BotFlip.cpp
@@ -14,12 +14,6 @@
 #include <stdio.h>
 
 
-// #include <opencv2/core/core.hpp>
-
-
-// #include <opencv2/highgui/highgui.hpp>    
-
-
 using namespace std;
 
 // Class
@@ -256,13 +250,13 @@ public:
 		try
 		{
 			// Set charecters to spam
-			char chars[100] = {
+			char chars[115] = {
 
 			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 			'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', ';', ',', '.', '/', '~', '!', '@', '#', '$', '%', '^',
 			'&', '*', '(', ')', '_', '+', '{', '}', '|', ':', '<', '>', '?', 'Q', 'W', 'E', 'R', 'T', 'Y,', 'U', 'I', 'O', 'P', 'A', 'S', 'D',
 			'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ' ', '\u0201', '\u00a1', '\u00a2', '\u00a3', '\u00a4', '\u00a5',
-			'\u00a6', '\u00a7'
+			'\u00a6', '\u00a7', '', 'é', 'Ó', 'Ñ', '¼', 'Ì', 'ó', 'ÿ', 'ÿ', 'ƒ', 'Ä', 'Ê', 'ô', 'å', 'ÿ'
 
 			};
 
@@ -276,7 +270,7 @@ public:
 					// Spam
 
 					//Get char
-					char letter = chars[rand() % 101 + 1];
+					char letter = chars[rand() % 100 + 1];
 
 					// Print char
 					cout << letter;
