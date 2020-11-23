c61cd4b129a0f6f4436804a5c1e80abee2cee774
RichVRed <richvred@gmail.com>
2020-11-23 07:49:09 -0800

    [feature] implement isYes and isNo boolean string checks to centralize logic, updated fowrite to continually append input until a keyword is reached and formatted the help command code

--- BotFlip.cpp
+++ BotFlip.cpp
@@ -96,45 +96,44 @@ public:
 	{
 		try
 		{
-
 			// Help
 			cout << "--------------------HELP--------------------" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "[command name]" << std::setw(helpDescriptionWidth) << "[description]" << std::setw(helpIdWidth) << "[id]" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "help" << std::setw(helpDescriptionWidth) << "Displays this help message" << std::setw(helpIdWidth) << "hlp" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "playCoins" << std::setw(helpDescriptionWidth) << "Play for a chance to get coins!" << std::setw(helpIdWidth) << "plyCns" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "playPaper" << std::setw(helpDescriptionWidth) << "Play for a chance to get paper!" << std::setw(helpIdWidth) << "plyPpr" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "coins" << std::setw(helpDescriptionWidth) << "See all the coins you have" << std::setw(helpIdWidth) << "cns" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "paper" << std::setw(helpDescriptionWidth) << "See all the paper you have" << std::setw(helpIdWidth) << "ppr" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "sell" << std::setw(helpDescriptionWidth) << "Sell the paper you have" << std::setw(helpIdWidth) << "sll" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "steal" << std::setw(helpDescriptionWidth) << "Steal" << std::setw(helpIdWidth) << "stl" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "givemecoffee" << std::setw(helpDescriptionWidth) << "Gives you coffee" << std::setw(helpIdWidth) << "gvmcff" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "cls" << std::setw(helpDescriptionWidth) << "Clears all the text on the screen" << std::setw(helpIdWidth) << "clear" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "spam" << std::setw(helpDescriptionWidth) << "Program spams" << std::setw(helpIdWidth) << "spm" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "exit" << std::setw(helpDescriptionWidth) << "Exits this program" << std::setw(helpIdWidth) << "ext" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "add" << std::setw(helpDescriptionWidth) << "Adds two numbers" << std::setw(helpIdWidth) << "add" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "subtract" << std::setw(helpDescriptionWidth) << "Sutracts two numbers" << std::setw(helpIdWidth) << "sbtrct" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "multiply" << std::setw(helpDescriptionWidth) << "Multiplies two numbers" << std::setw(helpIdWidth) << "mul" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "divide" << std::setw(helpDescriptionWidth) << "Divides two numbers" << std::setw(helpIdWidth) << "div" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "modulus" << std::setw(helpDescriptionWidth) << "Calculates the modules (mod) of two numbers" << std::setw(helpIdWidth) << "mod" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "sine" << std::setw(helpDescriptionWidth) << "Calculates sine of on a number" << std::setw(helpIdWidth) << "sin" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "cosine" << std::setw(helpDescriptionWidth) << "Calculates cosine on a number" << std::setw(helpIdWidth) << "cos" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "tangent" << std::setw(helpDescriptionWidth) << "Calculates tangent on a number" << std::setw(helpIdWidth) << "tan" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "arcsin" << std::setw(helpDescriptionWidth) << "Calculates arcsin on a number" << std::setw(helpIdWidth) << "asin" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "arccos" << std::setw(helpDescriptionWidth) << "Calculates arccos on a number" << std::setw(helpIdWidth) << "acos" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "arctan" << std::setw(helpDescriptionWidth) << "Calculates arctan on a number" << std::setw(helpIdWidth) << "atan" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "ver" << std::setw(helpDescriptionWidth) << "Displays the version" << std::setw(helpIdWidth) << "ver" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "spcfcVer" << std::setw(helpDescriptionWidth) << "Displays the specific version" << std::setw(helpIdWidth) << "sVer" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "repository" << std::setw(helpDescriptionWidth) << "Opens BotFlip's repository" << std::setw(helpIdWidth) << "repo" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "report" << std::setw(helpDescriptionWidth) << "Opens issue panel where you can report an issue" << std::setw(helpIdWidth) << "bug" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "releases" << std::setw(helpDescriptionWidth) << "Opens all the releases" << std::setw(helpIdWidth) << "release" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "debug" << std::setw(helpDescriptionWidth) << "Basic debug" << std::setw(helpIdWidth) << "debug" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "fwrite" << std::setw(helpDescriptionWidth) << "File write, opens a file a writes some text into it" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "fowrite" << std::setw(helpDescriptionWidth) << "File overwrite, opens a file and overwrites some text into it" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "fclear" << std::setw(helpDescriptionWidth) << "File clear, opens a file and clears all the contents in the file" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "color-green" << std::setw(helpDescriptionWidth) << "Sets the text color to green (default)" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "color-red" << std::setw(helpDescriptionWidth) << "Sets the text color to red" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "color-white" << std::setw(helpDescriptionWidth) << "Sets the text color to white" << std::endl;
-			cout << std::left << std::setw(helpCommandWidth) << "color-blue" << std::setw(helpDescriptionWidth) << "Sets the text color to blue (not recommended)" << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "[command name]" << std::setw(helpDescriptionWidth) << "[description]"                                                    << std::setw(helpIdWidth) << "[id]"    << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "help"           << std::setw(helpDescriptionWidth) << "Displays this help message"                                       << std::setw(helpIdWidth) << "hlp"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "playCoins"      << std::setw(helpDescriptionWidth) << "Play for a chance to get coins!"                                  << std::setw(helpIdWidth) << "plyCns"  << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "playPaper"      << std::setw(helpDescriptionWidth) << "Play for a chance to get paper!"                                  << std::setw(helpIdWidth) << "plyPpr"  << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "coins"          << std::setw(helpDescriptionWidth) << "See all the coins you have"                                       << std::setw(helpIdWidth) << "cns"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "paper"          << std::setw(helpDescriptionWidth) << "See all the paper you have"                                       << std::setw(helpIdWidth) << "ppr"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "sell"           << std::setw(helpDescriptionWidth) << "Sell the paper you have"                                          << std::setw(helpIdWidth) << "sll"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "steal"          << std::setw(helpDescriptionWidth) << "Steal"                                                            << std::setw(helpIdWidth) << "stl"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "givemecoffee"   << std::setw(helpDescriptionWidth) << "Gives you coffee"                                                 << std::setw(helpIdWidth) << "gvmcff"  << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "cls"            << std::setw(helpDescriptionWidth) << "Clears all the text on the screen"                                << std::setw(helpIdWidth) << "clear"   << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "spam"           << std::setw(helpDescriptionWidth) << "Program spams"                                                    << std::setw(helpIdWidth) << "spm"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "exit"           << std::setw(helpDescriptionWidth) << "Exits this program"                                               << std::setw(helpIdWidth) << "ext"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "add"            << std::setw(helpDescriptionWidth) << "Adds two numbers"                                                 << std::setw(helpIdWidth) << "add"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "subtract"       << std::setw(helpDescriptionWidth) << "Sutracts two numbers"                                             << std::setw(helpIdWidth) << "sbtrct"  << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "multiply"       << std::setw(helpDescriptionWidth) << "Multiplies two numbers"                                           << std::setw(helpIdWidth) << "mul"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "divide"         << std::setw(helpDescriptionWidth) << "Divides two numbers"                                              << std::setw(helpIdWidth) << "div"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "modulus"        << std::setw(helpDescriptionWidth) << "Calculates the modules (mod) of two numbers"                      << std::setw(helpIdWidth) << "mod"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "sine"           << std::setw(helpDescriptionWidth) << "Calculates sine of on a number"                                   << std::setw(helpIdWidth) << "sin"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "cosine"         << std::setw(helpDescriptionWidth) << "Calculates cosine on a number"                                    << std::setw(helpIdWidth) << "cos"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "tangent"        << std::setw(helpDescriptionWidth) << "Calculates tangent on a number"                                   << std::setw(helpIdWidth) << "tan"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "arcsin"         << std::setw(helpDescriptionWidth) << "Calculates arcsin on a number"                                    << std::setw(helpIdWidth) << "asin"    << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "arccos"         << std::setw(helpDescriptionWidth) << "Calculates arccos on a number"                                    << std::setw(helpIdWidth) << "acos"    << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "arctan"         << std::setw(helpDescriptionWidth) << "Calculates arctan on a number"                                    << std::setw(helpIdWidth) << "atan"    << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "ver"            << std::setw(helpDescriptionWidth) << "Displays the version"                                             << std::setw(helpIdWidth) << "ver"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "spcfcVer"       << std::setw(helpDescriptionWidth) << "Displays the specific version"                                    << std::setw(helpIdWidth) << "sVer"    << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "repository"     << std::setw(helpDescriptionWidth) << "Opens BotFlip's repository"                                       << std::setw(helpIdWidth) << "repo"    << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "report"         << std::setw(helpDescriptionWidth) << "Opens issue panel where you can report an issue"                  << std::setw(helpIdWidth) << "bug"     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "releases"       << std::setw(helpDescriptionWidth) << "Opens all the releases"                                           << std::setw(helpIdWidth) << "release" << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "debug"          << std::setw(helpDescriptionWidth) << "Basic debug"                                                      << std::setw(helpIdWidth) << "debug"   << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "fwrite"         << std::setw(helpDescriptionWidth) << "File write, opens a file a writes some text into it"                                                     << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "fowrite"        << std::setw(helpDescriptionWidth) << "File overwrite, opens a file and overwrites some text into it"                                           << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "fclear"         << std::setw(helpDescriptionWidth) << "File clear, opens a file and clears all the contents in the file"                                        << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "color-green"    << std::setw(helpDescriptionWidth) << "Sets the text color to green (default)"                                                                  << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "color-red"      << std::setw(helpDescriptionWidth) << "Sets the text color to red"                                                                              << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "color-white"    << std::setw(helpDescriptionWidth) << "Sets the text color to white"                                                                            << std::endl;
+			cout << std::left << std::setw(helpCommandWidth) << "color-blue"     << std::setw(helpDescriptionWidth) << "Sets the text color to blue (not recommended)"                                                           << std::endl;
 			cout << std::endl;
 			cout << "-------------------------------------------------" << std::endl;
 		}
@@ -564,12 +563,12 @@ public:
 			cin >> report;
 
 			// Check
-			if (report == "Y" || report == "y")
+			if (isYes(report))
 			{
 				// Report bug
 				bug();
 			}
-			else if (report == "N" || report == "n")
+			else if (isNo(report))
 			{
 				// Check for updates
 				cout << "Do you want to check for updates?\nY = yes | N = No\n";
@@ -581,12 +580,12 @@ public:
 				cin >> update;
 
 				// Check
-				if (update == "Y" || update == "y")
+				if (isYes(report))
 				{
 					// Open releases page on Github
 					releases();
 				}
-				else if (update == "N" || update == "n")
+				else if (isNo(report))
 				{
 					// Do nothing
 				}
@@ -604,22 +603,37 @@ public:
 		}
 	}
 
+	bool isYes(string character)
+	{
+		return ("Y" == character || "y" == character);
+	}
+
+	bool isNo(string character)
+	{
+		return ("N" == character || "n" == character);
+	}
+
 	// File write (fwrite)
 	void fwrite()
 	{
 		ofstream text;
 
 		// Ask
-		cout << "To Write?\n";
+		cout << "When finished writing type CLOSE on a new line. What do you want to write?\n";
 
 		// Get input
 		cin >> toWrite;
 
-		// Open file
+		// Open file and append new content
 		text.open("text.txt", ios_base::app);
 
-		// Update file
-		text << toWrite;
+		while (getline(cin, toWrite)) {
+			if (toWrite == "CLOSE") {
+				break;
+			}
+			// Update file
+			text << toWrite << std::endl;
+		}
 
 		// Close file
 		text.close();
@@ -637,11 +651,16 @@ public:
 		// Input
 		cin >> toWrite;
 
-		// Open file
-		text.open("text.txt");
+		// Open file and truncate content
+		text.open("text.txt", ios::trunc);
 
-		// Update file
-		text << toWrite + "\n";
+		while (getline(cin, toWrite)) {
+			if (toWrite == "CLOSE") {
+				break;
+			}
+			// Update file
+			text << toWrite << std::endl;
+		}
 
 		// Close
 		text.close();
@@ -653,8 +672,8 @@ public:
 	{
 		ofstream text;
 
-		// Open text.txt
-		text.open("text.txt");
+		// Open file and truncate previous content
+		text.open("text.txt", ios::trunc);
 
 		// Clear
 		text << " ";
@@ -668,7 +687,7 @@ public:
 	{
 		ofstream cmd_log;
 
-		// Open log
+		// Open log and append new content
 		cmd_log.open("cmd_log.txt", ios_base::app);
 
 		// Update log
